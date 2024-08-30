cbb8cc872220604fc2040e578c471e36522c9eab
Vladislav Harchenko <vladislav.harchenko2017@yandex.ru>
2024-08-31 00:29:44 +0700

    update(progress-container.css): add animated & hidden styles, font-weight update update(progress-container.js): add input & checkbox scripts

--- progress-container.js
+++ progress-container.js
@@ -1,1 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const progressCircle = document.getElementById("progress-circle");
+  const progressValueInput = document.getElementById("progress-value");
+  const animateCheckbox = document.getElementById("animate");
+  const hideCheckbox = document.getElementById("hide");
+  const progressContainerLoader = document.querySelector(
+    ".progress-container-loader"
+  );
 
+  let lastInputValue = progressValueInput.value;
+
+  progressValueInput.addEventListener("input", function () {
+    let value = progressValueInput.value;
+
+    if (parseInt(value) > 100) {
+      value = lastInputValue;
+    }
+
+    if (value.length > 3) {
+      value = value.slice(0, 3);
+    }
+
+    if (value.length > 1 && value.startsWith("0")) {
+      value = value.slice(1);
+    }
+
+    lastInputValue = value;
+    value = Math.min(100, Math.max(0, parseInt(value) || 0));
+    progressValueInput.value = value;
+    const offset = (value / 100) * 132;
+    progressCircle.style.strokeDasharray = `${offset}, 132`;
+  });
+
+  animateCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+      progressCircle.classList.add("animated");
+    } else {
+      progressCircle.classList.remove("animated");
+    }
+  });
+
+  hideCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+      progressContainerLoader.classList.add("hidden");
+    } else {
+      progressContainerLoader.classList.remove("hidden");
+    }
+  });
+});
